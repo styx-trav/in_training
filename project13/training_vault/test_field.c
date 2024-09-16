df0a5761d6a7b76dbc9ae0291f7bfaa3eaa100c9
styx-trav <naomi.rakotomanga@gmail.com>
2024-09-16 16:18:24 +0200

    worked a little on those tree structures and i get it now, i retract all that sh i said theyre actually really fun

--- project13/training_vault/test_field.c
+++ project13/training_vault/test_field.c
@@ -21,6 +21,8 @@ t_btree *btree_create_node(void *item)
     return (NULL);
   }
   new_thing->item = item;
+  new_thing->left = NULL;
+  new_thing->right = NULL;
   return (new_thing);
 }
 
@@ -55,7 +57,7 @@ void  btree_apply_prefix(t_btree *root, void (*applyf)(char *))
   }
 }
 
-void  disp_tree(t_btree *root)
+void  disp_tree_preorder(t_btree *root)
 {
   t_btree *current;
 
@@ -63,8 +65,77 @@ void  disp_tree(t_btree *root)
   if (current != NULL)
   {
     printf("here string: %s,\n", current->item);
-    disp_tree(current->right);
-    disp_tree(current->left);
+    disp_tree_preorder(current->left);
+    disp_tree_preorder(current->right);
+  }
+}
+
+void  disp_tree_inorder(t_btree *root)
+{
+  t_btree *current;
+
+  current = root;
+  if (current != NULL)
+  {
+    disp_tree_inorder(current->left);
+    printf("here string: %s,\n", current->item);
+    disp_tree_inorder(current->right);
+  }
+}
+
+void  disp_tree_postorder(t_btree *root)
+{
+  t_btree *current;
+
+  current = root;
+  if (current != NULL)
+  {
+    disp_tree_postorder(current->left);
+    disp_tree_postorder(current->right);
+    printf("here string: %s,\n", current->item);
+  }
+}
+
+void  disp_tree_bylevel(t_btree *root, int i, int lev)
+{
+  t_btree *current;
+
+  current = root;
+  if (i == lev && current != NULL)
+    printf("here string: %s,\n", current->item);
+  if (current != NULL)
+  {
+    disp_tree_bylevel(current->left, i + 1, lev);
+    disp_tree_bylevel(current->right, i + 1, lev);
+  }
+}
+
+int tree_depth(t_btree  *root, int lev)
+{
+  int lev1;
+  int lev2;
+
+  if (root != NULL)
+  {
+    lev1 = tree_depth(root->left, lev + 1);
+    lev2 = tree_depth(root->right, lev + 1);
+    if (lev1 < lev2)
+      return (lev2);
+    else
+      return (lev1);
+  }
+  return (lev);
+}
+
+void  disp_tree_levels(t_btree *root, int depth)
+{
+  int i;
+
+  i = 0;
+  while (i < depth)
+  {
+    disp_tree_bylevel(root, 0, i);
+    i++;
   }
 }
 
@@ -75,26 +146,23 @@ int main(int argc, char **argv)
   t_btree *tmp2;
 
   head = btree_create_node(argv[1]);
-  head->left = NULL;
-  head->right = NULL;
-  tmp1 = head;
-  head = btree_create_node(argv[2]);
-  head->left = NULL;
-  head->right = NULL;
-  tmp2 = head;
-  head = btree_create_node(argv[3]);
-  head->left = tmp1;
-  head->right = tmp2;
-  tmp1 = head;
-  head = btree_create_node(argv[4]);
-  head->left = NULL;
-  head->right = tmp1;
-  tmp2 = head;
-  head = btree_create_node(argv[5]);
-  head->left = tmp2;
-  head->right = NULL;
-  disp_tree(head);
-  btree_apply_prefix(head, &applyf);
-  disp_tree(head);
+  head->left = btree_create_node(argv[2]);
+  head->right = btree_create_node(argv[3]);
+  head->left->right = btree_create_node(argv[4]);
+  head->right->left = btree_create_node(argv[5]);
+  head->right->right = btree_create_node(argv[6]);
+  head->left->left = btree_create_node(argv[7]);
+  head->left->left->right = btree_create_node(argv[8]);
+  head->right->left->left = btree_create_node(argv[9]);
+  head->right->left->right = btree_create_node(argv[10]);
+  head->right->left->right->left = btree_create_node(argv[11]);
+  disp_tree_preorder(head);
+  //btree_apply_prefix(head, &applyf);
+  printf("\n");
+  disp_tree_inorder(head);
+  printf("\n");
+  disp_tree_postorder(head);
+  printf("\n, in levels:\n\n");
+  disp_tree_levels(head, tree_depth(head, 0));
   free_tree(head);
 }
